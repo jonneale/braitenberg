087472303488c3ea79ee8d0b6d6c06cc6c59baef
Jon Neale <jon.neale@USW-00589.local>
2017-03-14 12:46:56 +0000

    Pushing and popping

--- src/braitenberg/drawing.clj
+++ src/braitenberg/drawing.clj
@@ -7,15 +7,10 @@
 (def width 500)
 (def height 500)
 
-(defn setup [initial-state]
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  initial-state)
-
-(def max-speed 0.05)
+(def max-speed 0.5)
 
 (defn update-attitude
-  [ attitude]
+  [attitude]
   attitude)
 
 (defn cap-speed
@@ -46,7 +41,7 @@
 
 (defn new-speed
   [initial-speed]
-  (+ initial-speed (/ (- (rand) 0.5) 10)))
+  initial-speed)
 
 (defn update-vehicle
   [frame-rate vehicle]
@@ -55,8 +50,8 @@
     (merge vehicle
            {:x new-x
             :y new-y
-            :left-wheel-speed  (cap-speed max-speed frame-rate (new-speed left-wheel-speed))
-            :right-wheel-speed (cap-speed max-speed frame-rate (new-speed right-wheel-speed))
+            :left-wheel-speed  left-wheel-speed
+            :right-wheel-speed right-wheel-speed
             :attitude          new-attitude})))
 
 (defn update-vehicles
@@ -72,21 +67,9 @@
   [(- (/ (* width   (:axle-width vehicle)) 2.0))
    (- (/ (* height  (:axle-width vehicle)) 2.0))])
 
-(defn set-origin-to-centre!
-  [vehicle]
-  (let [[centre-x centre-y] (centre vehicle)]
-       (q/translate (+ (translate-coord (:x vehicle) width)
-                       centre-x) 
-                    (+ (translate-coord (:y vehicle) height)
-                       centre-y))))
-
 (defn- translate-coord
   [coord max-value]
   (min max-value (max 0 (* coord max-value))))
-
-
-;; (+ centre-x 15) (- 0 15 centre-y)  
-;;                 (- centre-x 15) (- 0 15 centre-y)
 
 (defn display-sensors
   [vehicle]
@@ -94,24 +77,23 @@
         [centre-x centre-y] (centre vehicle)]
     (q/no-fill)
     (q/stroke 255 0 0)
-    (q/triangle (- 0 w centre-x) (- 0 w centre-y) 
-                (+ (- 0 centre-x) w) (- 0 w centre-y) 
-                (- centre-x) (- centre-y))
-    (q/triangle (- 0 w (* 3 centre-x)) (- 0 w centre-y) 
-                (+ (- 0 (* 3 centre-x)) w) (- 0 w centre-y) 
-                (- 0 (* 3 centre-x)) (- centre-y))
+    ;; (q/triangle (- 0 w centre-x) (- 0 w centre-y) 
+    ;;             (+ (- 0 centre-x) w) (- 0 w centre-y) 
+    ;;             (- centre-x) (- centre-y))
+    ;; (q/triangle (- 0 w (* 3 centre-x)) (- 0 w centre-y) 
+    ;;             (+ (- 0 (* 3 centre-x)) w) (- 0 w centre-y) 
+    ;;             (- 0 (* 3 centre-x)) (- centre-y))
     (q/stroke 0 255 0)
     (q/ellipse 0 0 
                (translate-coord (:detectable-radius vehicle) width)
                (translate-coord (:detectable-radius vehicle) width))))
 
 (defn draw-state [{:keys [vehicles frame-rate display-radius] :as state}]
-                                        ; Clear the sketch by filling it with light-grey color.
   (q/background 255)
   (q/frame-rate frame-rate)
-  (q/reset-matrix)
   (doseq [vehicle vehicles]
     (let [[centre-x centre-y] (centre vehicle)]
+      (q/push-matrix)
       (q/translate (+ (translate-coord (:x vehicle) width)
                       centre-x) 
                    (+ (translate-coord (:y vehicle) height)
@@ -119,28 +101,24 @@
       (q/rotate (to-radians (:attitude vehicle)))
       (when display-radius (display-sensors vehicle))
       (q/stroke 0 0 0)
-      (q/rect (- centre-x)
-              (- centre-y)
+      (q/rect centre-x
+              centre-y
               (* width  (:axle-width vehicle))
               (* height (:axle-width vehicle)))
-      (q/reset-matrix))))
+      (q/pop-matrix))))
+
 
 (defn run
   ([]
    (run (core/reset-state)))
-  ([vehicles]
+  ([initial-state]
    (q/defsketch braitenberg
      :title "You spin my circle right round"
      :size [width height]
-                                        ; setup function called only once, during sketch initialization.
-     :setup #(setup vehicles)
-                                        ; update-state is called on each iteration before draw-s
-     :update identity;;update-state
+     :setup  (constantly initial-state)
+     :update update-state
      :draw   draw-state
      :features [:keep-on-top]
-                                        ; This sketch uses functional-mode middleware.
-                                        ; Check quil wiki for more info about middlewares and particularly
-                                        ; fun-mode.
      :middleware [m/fun-mode]
      :key-pressed keyboard/key-pressed)))
 
